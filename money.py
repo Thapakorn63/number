dd560657274a8198fbfc14d550783a79fa6dd29e
Thapakorn63 <68268428+Thapakorn63@users.noreply.github.com>
2020-07-29 10:21:48 +0700

    Add files via upload

---
+++ money.py
@@ -0,0 +1,17 @@
+number =int(input("Enter number : "))
+num1=number//1000
+number=number%1000
+
+num2=number//100
+number=number%100
+
+num3=number//10
+number=number%10
+
+num4=number//1
+number=number%1
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
